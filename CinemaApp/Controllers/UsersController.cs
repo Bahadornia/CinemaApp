2b3fab1f3f3ed85443760194221336a53dc964a9
b.bahadornia <b.bahadornia@fwutech.com>
2022-05-28 22:41:25 +0430

    Added register method.

---
+++ CinemaApp/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private CinemaAppDbContext _dbContext;
+
+        public UsersController(CinemaAppDbContext dbContext)
+        {
+
+            _dbContext = dbContext;
+
+        }
+
+        public IActionResult Register([FromBody] User user)
+        {
+
+            var userWithSameEmail = _dbContext.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+            if(userWithSameEmail != null)
+            {
+                return BadRequest("User already exists!");
+            }
+            else
+            {
+                var userObj = new User
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    Password = user.Password,
+                    Role = user.Role,
+
+                };
+
+                _dbContext.Users.Add(userObj);
+                _dbContext.SaveChanges();
+            }
+            return StatusCode(StatusCodes.Status201Created);
+
+        }
+    }
+}
--- CinemaApp/Models/User.cs
+++ CinemaApp/Models/User.cs
@@ -9,6 +9,6 @@
         public string Password { get; set; }
         public string Role { get; set; }
 
-        public ICollection<Reservation> Reservations { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
     }
 }
